5a0b828d89cdf49f05cd8797e3d281f8fcca01e2
MajaPy <mpytlik@student.agh.edu.pl>
2026-05-24 10:00:29 +0200

    nr5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite
+
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+
+
   ],
   base: '/zad5/', 
 })
